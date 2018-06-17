1f1780a0b9d7b760927586828df1d26bfd2a44f1
marco.sciarra <marco.sciarra@clicksrl.eu>
2018-06-17 18:41:50 +0200

    dragAndDrop + Ng-toast

---
+++ ngToast/ngToast/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html ng-app="App" ng-controller="indexController">
+
+<head>
+<!-- import bootstrap 3 css -->
+<link href="../grafica/bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css" media="all">
+<!-- import angularjs -->
+<script type="text/javascript" src="../_lib/angularlib/angular.min.js"></script>
+<script type="text/javascript" src="../_lib/angularlib/angular-animate.js"></script>
+<script type="text/javascript" src="../_lib/angularlib/angular-sanitize.js"></script>
+
+<!--jquery-->
+<script type="text/javascript" src="../_lib/utilityLib/jquery.min.js"></script>
+<!-- fontAwesome -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<!--import bootstrap js [NON SPOSTARE]-->
+<script type="text/javascript" src="../grafica/bootstrap/js/bootstrap.min.js"></script>
+
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+
+<body class="bgColor">
+
+<div>
+
+</div>
+
+</body>
+</html>
