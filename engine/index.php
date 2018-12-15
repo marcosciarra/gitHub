6b65e849afb623d3b6800e71aa5639abf87441c8
marco.sciarra <marco.sciarra@clicksrl.eu>
2018-12-15 22:25:28 +0100

    lettura struttura tabelle

--- engine/index.php
+++ engine/index.php
@@ -12,14 +12,53 @@ header("Content-type: text/html; charset=latin1");
 require_once 'conf/costanti.php';
 require_once 'lib/pdo.php';
 
-$attribute = array(PDO::ATTR_EMULATE_PREPARES => false, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
-$pdo = new PDO(
-    'mysql:host=' . HOST . ';dbname=' . SCHEMA . ';charset=' . CHARSET,
-    USER,
-    PWD,
-    $attribute
-);
+//$attribute = array(PDO::ATTR_EMULATE_PREPARES => false, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
+//$pdo = new PDO(
+//    'mysql:host=' . HOST . ';dbname=' . SCHEMA . ';charset=' . CHARSET,
+//    USER,
+//    PWD,
+//    $attribute
+//);
+//
+//$pdo->query("select * from contratti");
+//
+//var_dump($pdo);
 
-$struttura = $pdo->query("SHOW TABLE STATUS FROM " . SCHEMA);
+try {
+    $attribute = array(PDO::ATTR_EMULATE_PREPARES => false, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
+    $pdo = new PDO(
+        'mysql:host=' . HOST . ';dbname=' . SCHEMA . ';charset=' . CHARSET,
+        USER,
+        PWD,
+        $attribute
+    );
 
-var_dump($struttura);
+    echo '<br><br><br>ELENCO TABELLE<br><br><br>';
+    $query='SHOW TABLES';
+
+    foreach ($pdo->query($query)->fetchAll() as $app){
+        var_dump($app);
+        echo '<br><br><br>';
+    }
+
+    echo '<br><br><br>STRUTTURA TABELLA<br><br><br>';
+
+    $query='DESCRIBE contratti';
+
+    foreach ($pdo->query($query)->fetchAll() as $app){
+        var_dump($app);
+        echo '<br><br><br>';
+    }
+
+    echo '<br><br><br>INDICI<br><br><br>';
+
+    $query='SHOW INDEX FROM contratti';
+
+    foreach ($pdo->query($query)->fetchAll() as $app){
+        var_dump($app);
+        echo '<br><br><br>';
+    }
+} catch (PDOException $e) {
+    die('Errore durante la connessione al database!: ' . $e->getMessage());
+}
+
