48dc9093bae963eb7be101e44bc726eaa0b3ebe3
marco.sciarra <marco.sciarra@clicksrl.eu>
2019-01-15 15:46:40 +0100

    wip

--- dashboard/conf/conf.php
+++ dashboard/conf/conf.php
@@ -27,16 +27,3 @@ define("USER1", "root");
 define("PWD1", "Sciarra82");
 define("SCHEMA1", "dashboard");
 define('CHARSET1', 'latin1');
-
-//URL
-define("BASE_URL", "http://localhost");
-//define("BASE_URL", "http://172.16.0.200:83");
-
-//CONFIGURAZIONE SMTP
-define("EMAIL_USE_SMTP", true);
-define("EMAIL_SMTP_HOST", "smtp-relay.sendinblue.com");
-define("EMAIL_SMTP_AUTH", true);
-define("EMAIL_SMTP_USERNAME", "support@clicksrl.eu");
-define("EMAIL_SMTP_PASSWORD", "0vNtV2IwUnxWD9hY");
-define("EMAIL_SMTP_PORT", 587);
-define("EMAIL_SMTP_ENCRYPTION", "");
