1e30500d82fe1a2e7ae2f089a54ae2c5f7f488be
marcosciarra <32035287+marcosciarra@users.noreply.github.com>
2019-11-06 22:13:34 +0100

    Add array

---
+++ JS/corso/js/array.js
@@ -0,0 +1,14 @@
+//Array ==> Gli array in Javascript sono Object
+
+//Dichiarazione
+var giorni= new Array();    //Dichiarazione di un array    
+var mesi= new Array(12);    //Array con 12 valori ad undefined
+var anni =[];               //Altro modo per dichiarare array
+
+//Assegno valori
+var prova=[];
+prova=[1,true,'tre',[1,2,3,4]];
+
+//Copia per riferimento
+var prova2=prova;       //Questa è una copia di array per riferimento => se modifica prova2 modifico anche prova
+
